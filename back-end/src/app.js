f522175049b05d9e9b3025bc3f7b27dd0b6764bb
QWARDZ <johnedwardbearnezaa@gmail.com>
2026-03-25 09:54:05 +0800

    update

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -6,7 +6,10 @@ const employeeRoutes = require("./routes/employeeRoutes");
 const propertyRoutes = require("./routes/propertyRoutes");
 const authRoutes = require("./routes/authRoutes");
 const adminRoutes = require("./routes/adminRoutes");
+<<<<<<< HEAD
+=======
 // const propertyRoutes = require("./routes/propertyRoutes");
+>>>>>>> main
 const lotRoutes = require("./routes/lotRoutes");
 const clientRoutes = require("./routes/clientRoutes");
 
@@ -25,6 +28,19 @@ app.use(express.urlencoded({ extended: true }));
 
 // Session Configuration
 app.use(
+<<<<<<< HEAD
+	session({
+		secret: process.env.SESSION_SECRET,
+		resave: false,
+		saveUninitialized: false,
+		cookie: {
+			maxAge: 2 * 60 * 60 * 1000, // 2 hours
+			httpOnly: true,
+			sameSite: "lax",
+			secure: false, // Set to true if using HTTPS
+		},
+	}),
+=======
   session({
     secret: process.env.SESSION_SECRET,
     resave: false,
@@ -36,6 +52,7 @@ app.use(
       secure: false, // Set to true if using HTTPS
     },
   })
+>>>>>>> main
 );
 
 // Test route
@@ -53,11 +70,11 @@ app.use("/api/admin", sessionOrToken({ roles: ["admin"] }), adminRoutes);
 
 // 404 handler
 app.use((req, res) => {
-  res.status(404).json({
-    error: "Service Unavailable",
-    message: "The requested resource was not found",
-    status: 404,
-  });
+	res.status(404).json({
+		error: "Service Unavailable",
+		message: "The requested resource was not found",
+		status: 404,
+	});
 });
 
 module.exports = app;
